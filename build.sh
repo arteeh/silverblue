f53520c998379878f787a14240d8b8b8ea95d482
Maarten de Jong <35239587+arteeh@users.noreply.github.com>
2025-03-10 14:21:36 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ curl -Lo /etc/yum.repos.d/tailscale.repo https://pkgs.tailscale.com/stable/fedor
 
 dnf5 install -y tailscale
 
-dnf5 remote -y ptyxis yelp firefox firefox-langpacks gnome-system-monitor gnome-tweaks gnome-tour gnome-classic-session gnome-shell-extension-apps-menu gnome-shell-extension-background-logo gnome-shell-extension-launch-new-instance gnome-shell-extension-places-menu gnome-shell-extension-window-list
+dnf5 remove -y ptyxis yelp firefox firefox-langpacks gnome-system-monitor gnome-tweaks gnome-tour gnome-classic-session gnome-shell-extension-apps-menu gnome-shell-extension-background-logo gnome-shell-extension-launch-new-instance gnome-shell-extension-places-menu gnome-shell-extension-window-list
