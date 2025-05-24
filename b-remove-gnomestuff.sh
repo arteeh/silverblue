3e693bdf8f5a648d06f01528ca5c9d848dc56d69
Maarten de Jong <35239587+arteeh@users.noreply.github.com>
2025-05-24 16:54:00 +0200

    try removing malcontent frontend only

--- b-remove-gnomestuff.sh
+++ b-remove-gnomestuff.sh
@@ -2,4 +2,4 @@
 
 set -ouex pipefail
 
-dnf5 remove -y ptyxis yelp malcontent gnome-system-monitor gnome-tweaks gnome-tour gnome-classic-session gnome-shell-extension-apps-menu gnome-shell-extension-background-logo gnome-shell-extension-launch-new-instance gnome-shell-extension-places-menu gnome-shell-extension-window-list
+dnf5 remove -y ptyxis yelp malcontent-control gnome-system-monitor gnome-tweaks gnome-tour gnome-classic-session gnome-shell-extension-apps-menu gnome-shell-extension-background-logo gnome-shell-extension-launch-new-instance gnome-shell-extension-places-menu gnome-shell-extension-window-list
